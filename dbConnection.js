b947d50420e48d8642002d27f48b13c095b154f9
copopoco <153973942+copopoco@users.noreply.github.com>
2024-03-23 10:25:58 +0000

    fix

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 const dbConnection = async ()=>{
     mongoose
-    .connect( String(process.env.MONGODB_URI) )
+    .connect("mongodb+srv://tushar_sajwan:tushar_sajwan@cluster0.yk09smk.mongodb.net/MERN_STACK_EVENT_MESSAGE" )
     .then(()=> {
         console.log("Connected to database!");
     })
